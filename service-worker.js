c98a6d79d1ea2feccb011a6481aa421dca2af8fd
rayoxzwer <justchilling1305>
2025-10-16 23:19:54 +0500

    final

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'travelorda-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json',
+  './images/place1.jpg',
+  './images/place2.jpg',
+  './images/place3.jpg'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate and remove old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+});
+
+// Fetch offline content
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
